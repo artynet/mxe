48f2851bef4948130aff75f6d64e868069cc74b6
Arturo Rinaldi <arty.net2@gmail.com>
2018-03-13 19:36:34 +0100

    ffmpeg-master : adding relase 9c24911 from trunk

---
+++ src/ffmpeg-master.mk
@@ -0,0 +1,67 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := ffmpeg-master
+$(PKG)_WEBSITE  := https://ffmpeg.org/
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 9c24911
+$(PKG)_CHECKSUM := ab35561b3458d14332925b433eb40509f3f53a3839784407d474a4d321e0d921
+$(PKG)_SUBDIR   := FFmpeg-$(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := https://github.com/artynet/FFmpeg/archive/$($(PKG)_FILE)
+$(PKG)_DEPS     := cc bzip2 gnutls lame libass libbluray libbs2b libcaca \
+                   libvpx opencore-amr opus sdl speex theora vidstab \
+                   vo-amrwbenc vorbis x264 xvidcore yasm zlib
+
+# DO NOT ADD fdk-aac OR openssl SUPPORT.
+# Although they are free softwares, their licenses are not compatible with
+# the GPL, and we'd like to enable GPL in our default ffmpeg build.
+# See docs/index.html#potential-legal-issues
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'https://ffmpeg.org/releases/' | \
+    $(SED) -n 's,.*ffmpeg-\([0-9][^>]*\)\.tar.*,\1,p' | \
+    grep -v 'alpha\|beta\|rc\|git' | \
+    $(SORT) -Vr | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)' && ./configure \
+        --cross-prefix='$(TARGET)'- \
+        --enable-cross-compile \
+        --arch=$(firstword $(subst -, ,$(TARGET))) \
+        --target-os=mingw32 \
+        --prefix='$(PREFIX)/$(TARGET)' \
+        $(if $(BUILD_STATIC), \
+            --enable-static --disable-shared , \
+            --disable-static --enable-shared ) \
+        --yasmexe='$(TARGET)-yasm' \
+        --disable-debug \
+        --disable-pthreads \
+        --enable-w32threads \
+        --disable-doc \
+        --enable-avresample \
+        --enable-gpl \
+        --enable-version3 \
+        --extra-libs='-mconsole' \
+        --enable-avisynth \
+        --enable-gnutls \
+        --enable-libass \
+        --enable-libbluray \
+        --enable-libbs2b \
+        --enable-libcaca \
+        --enable-libmp3lame \
+        --enable-libopencore-amrnb \
+        --enable-libopencore-amrwb \
+        --enable-libopus \
+        --enable-libspeex \
+        --enable-libtheora \
+        --enable-libvidstab \
+        --enable-libvo-amrwbenc \
+        --enable-libvorbis \
+        --enable-libvpx \
+        --enable-libx264 \
+        --enable-libxvid
+    $(MAKE) -C '$(1)' -j '$(JOBS)'
+    $(MAKE) -C '$(1)' -j 1 install
+endef
